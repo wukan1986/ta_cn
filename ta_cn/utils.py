70aaaa42dcdaa14a0390e63471a63ab11e597a61
伍侃 <wu-kan@163.com>
2022-09-04 16:27:40 +0800

    Update utils.py

--- ta_cn/utils.py
+++ ta_cn/utils.py
@@ -225,3 +225,120 @@ def round_a_a_i(func):
         return func(a, b, round(i))
 
     return decorated
+
+
+def dataframe_factorize(df, sort=False, na_sentinel=np.nan):
+    """将DataFrame整体编码。比astype('category')版快
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+    sort: bool
+        按出现先后编码，还是按排序编码。astype('category')版自动排序
+    na_sentinel: float or int
+        NaN的处理方式。pandas内部是-1, 这里改成支持nan
+
+    Returns
+    -------
+    codes: pd.Series or pd.DataFrame
+        编码
+    categories: pd.Index
+        分类关系
+
+    """
+
+    if np.isnan(na_sentinel):
+        codes, categories = pd.factorize(df.values.ravel(), sort=sort, na_sentinel=-1)
+        codes = np.where(codes == -1, np.nan, codes)
+    else:
+        codes, categories = pd.factorize(df.values.ravel(), sort=sort, na_sentinel=na_sentinel)
+
+    return np_to_pd(codes.reshape(df.shape), index=df.index, columns=df.columns), categories
+
+
+def resample_agg(df, index_func='last', agg_func='last', rule='15min', **resample_kwarg):
+    """对数据resample后再agg。时间标签来源于index，选择一周的最后一个交易日，而不是周日
+
+    Parameters
+    ----------
+    df
+    index_func: str or None
+        索引标签处理方法。None表示按resample_kwarg中的label设置
+    agg_func: str or dict
+        聚合函数
+    rule: str
+        采样规则
+    resample_kwarg
+        resample参数
+
+    Notes
+    -----
+    如果所有数据都是resample过后的，就算对齐时那天不是交易日也没关系，不影响结果
+
+    Examples
+    --------
+    >>> resample_agg(df, agg_func='sum', index_func='last', rule='W')
+    >>> resample_agg(df['IC9999.CCFX'], agg_func='sum', index_func='first', rule='W')
+    >>> resample_agg(df, index_func='last', agg_func='sum', rule='15min', closed='right')
+    >>> resample_agg(df, index_func=None, agg_func='sum', rule='8h', offset='4h', closed='right')
+
+    """
+    assert index_func in ('first', 'last', None)
+
+    dt = pd.Series(df.index, index=df.index)
+    if index_func is None:
+        # 对时间进行resample
+        dt = dt.resample(rule, **resample_kwarg).agg('first')
+        df = df.resample(rule, **resample_kwarg).agg(agg_func)
+        # 删除resample出现的NaT
+        return df.loc[dt.notna()]
+    else:
+        dt = dt.resample(rule, **resample_kwarg).agg(index_func)
+        df = df.resample(rule, **resample_kwarg).agg(agg_func)
+        df.index = dt
+        # 删除resample出现的NaT
+        return df.loc[df.index.notna()]
+
+
+def resample_agg_1d(df, index_func='last', agg_func='sum', groupby_agg_func='sum', floor_date=False, **resample_kwarg):
+    """resample得到日线数据
+
+    Parameters
+    ----------
+    df
+    index_func
+    agg_func
+    groupby_agg_func: str
+        分组合并方法。周六至周一将合并，不同的数据合并方法不一样。例如:
+            - open: first
+            - volume: sum
+            - low: min
+            - close: last
+    floor_date: bool
+        是否调整index将时间去除只保留日期
+    resample_kwarg
+
+    Examples
+    --------
+    >>> resample_agg_1d(df, index_func='last', agg_func='sum', groupby_agg_func='sum', floor_date=True, closed='right')
+
+    """
+    df_1d = resample_agg(df, index_func=index_func, agg_func=agg_func, rule='1d', offset='20h', **resample_kwarg)
+    # resample时间用的first也会移动到第二天
+    t = df_1d.index + pd.Timedelta(hours=4)
+    df_1d['_date_'] = t
+
+    # 周六日需要移动到下周一
+    is_w6_w7 = t.weekday >= 5
+
+    df_1d.loc[is_w6_w7, '_date_'] = pd.NaT
+    # return df_1d
+    df_1d['_date_'].bfill(inplace=True)
+
+    df_1d = df_1d.groupby(by='_date_').agg(groupby_agg_func)
+    # 时间还原
+    df_1d.index -= pd.Timedelta(hours=4)
+    # return df_1d
+    if floor_date:
+        df_1d.index = pd.to_datetime(df_1d.index.date)
+    return df_1d
