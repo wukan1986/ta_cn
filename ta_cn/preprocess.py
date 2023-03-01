6593c70715f2f761cd77e98ae18830d373bd8375
wukan <wu-kan@163.com>
2023-03-01 11:26:11 +0800

    屏蔽出现nan时，nanquantile报警告的问题

--- ta_cn/preprocess.py
+++ ta_cn/preprocess.py
@@ -57,6 +57,10 @@ def winsorize_quantile(x, min_=0.1, max_=0.9):
 
     """
     x = pd_to_np(x, copy=False)
+    # RuntimeWarning: All-NaN slice encountered r, k = function_base._ureduce(a,
+    if np.isnan(x).all():
+        return x
+
     axis = x.ndim - 1
     q = np.nanquantile(x, [min_, max_], axis=axis, keepdims=True)
     return np.clip(x, q[0], q[1])
